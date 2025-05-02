6d00e4f327217c490e2984c116d5f698a5f86148
Luminosity Prime <alphanovs0411@gmail.com>
2025-05-02 12:28:46 +0800

    header-fixed-final

--- gifty/client/.next/server/webpack-runtime.js
+++ gifty/client/.next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b5e35dfa32d04b15")
+/******/ 		__webpack_require__.h = () => ("e58bfcd7f393be22")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
